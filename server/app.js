b9afdd2f7d27e6a7411baafa919c5ffbd8ad0e78
Arsalan Khattak <me@khattak.dev>
2021-05-08 12:02:22 +0500

    add cors

--- server/app.js
+++ server/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const authRouter = require("./routes/auth");
 const productRouter = require("./routes/products");
 
 const app = express();
 
+app.use(cors());
 app.use("/auth", authRouter);
 app.use("/products", productRouter);
 
