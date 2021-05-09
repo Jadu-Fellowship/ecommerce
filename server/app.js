af852bbacf89af799650e44d527138d5d6754eb5
Arsalan Khattak <me@khattak.dev>
2021-05-09 12:02:33 +0500

    register/login with db

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
 const authRouter = require("./routes/auth");
 const productRouter = require("./routes/products");
@@ -6,10 +7,20 @@ const productRouter = require("./routes/products");
 const app = express();
 
 app.use(cors());
-// app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use("/auth", authRouter);
 app.use("/products", productRouter);
 
-app.listen(3001, console.log("Server Started!!"));
+makeConnection();
+
+async function makeConnection() {
+  await mongoose.connect(
+    "mongodb://127.0.0.1:27017/ecommerce?gssapiServiceName=mongodb",
+    { useNewUrlParser: true },
+    () => {
+      console.log("Database Connected!");
+      app.listen(3001, console.log("Server Started!!"));
+    }
+  );
+}
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,20 +1,56 @@
 const express = require("express");
-
+const User = require("../model/user");
 const router = express.Router();
 
-router.post("/register", (req, res) => {
-  console.log(req.query);
-  console.log(req.headers.authorization);
-  console.log(req.body);
-  res.json({
-    msg: "User Registered!",
-  });
+router.post("/register", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = new User({
+      email,
+      password,
+    });
+
+    await user.save();
+    res.json({
+      msg: "User Registered!",
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.json({
+      error: "Something unexpected happened!",
+    });
+  }
 });
 
-router.post("/login", (req, res) => {
-  res.json({
-    msg: "User Loggedin!",
-  });
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(401).json({
+        error: "Invalid Email",
+      });
+    }
+
+    console.log(user);
+    if (password == user.password) {
+      return res.json({
+        msg: "Password Matched!",
+      });
+    } else {
+      return res.json({
+        error: "Invalid Password",
+      });
+    }
+  } catch (error) {
+    console.log("Something unexpected!!", error.message);
+    return res.json({
+      error: "Server Error",
+    });
+  }
 });
 
 module.exports = router;
